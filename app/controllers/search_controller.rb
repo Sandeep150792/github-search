d650604b72c29d36f83071d84bb68f11d90e00e1
Sandeep Sharma <sanshar6@cisco.com>
2022-05-02 16:48:42 +0530

    Fixed indentation issue

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,10 @@ class SearchController < ApplicationController
   include RepoSearch
 
   def search_repo
-  	if params[:search].blank?
-  	  redirect_to action: 'index', flash: {error: "Please enter repo name"}
-	  else
-	    @repo_names = search(params[:search])
+    if params[:search].blank?
+      redirect_to action: 'index', flash: {error: "Please enter repo name"}
+    else
+      @repo_names = search(params[:search])
     end
   end
 end
--- app/services/repo_search.rb
+++ app/services/repo_search.rb
@@ -1,21 +1,21 @@
 module RepoSearch
 
   def search(query_param)
-	fetch_repo_details(get_result_request(query_param))
+    fetch_repo_details(get_result_request(query_param))
   end
 
   private
 
   def get_result_request(query_param)
-  	uri = URI("https://api.github.com/search/repositories")
-	param = {"q": query_param}
-	uri.query = URI.encode_www_form(param)
-	res = Net::HTTP.get_response(uri)
-	res = JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
-	res["items"]
+    uri = URI("https://api.github.com/search/repositories")
+    param = {"q": query_param}
+    uri.query = URI.encode_www_form(param)
+    res = Net::HTTP.get_response(uri)
+    res = JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+    res["items"]
   end
 
   def fetch_repo_details(items)
-  	items.map {|repo| {"name": repo["name"], "full_name": repo["full_name"], "url": repo["url"]}}
+    items.map {|repo| {"name": repo["name"], "full_name": repo["full_name"], "url": repo["url"]}}
   end
 end	
