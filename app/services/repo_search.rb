a1bf82dad59729fb9eb04c8984718bd8877d4e1b
Sandeep Sharma <sanshar6@cisco.com>
2022-05-02 18:03:47 +0530

    Added error handling

--- app/services/repo_search.rb
+++ app/services/repo_search.rb
@@ -1,18 +1,23 @@
 module RepoSearch
 
   def search(query_param)
-    fetch_repo_details(get_result_request(query_param))
+    get_result_request(query_param)
   end
 
   private
 
   def get_result_request(query_param)
-    uri = URI("https://api.github.com/search/repositories")
-    param = {"q": query_param}
-    uri.query = URI.encode_www_form(param)
-    res = Net::HTTP.get_response(uri)
-    res = JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
-    res["items"]
+    begin
+      uri = URI("https://api.github.com/search/repositories")
+      param = {"q": query_param}
+      uri.query = URI.encode_www_form(param)
+      res = Net::HTTP.get_response(uri)
+      res = JSON.parse(res.body)
+      fetch_repo_details(res["items"])
+    rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
+       Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError, SocketError => e
+       puts "Error in receiving the response"
+    end
   end
 
   def fetch_repo_details(items)
